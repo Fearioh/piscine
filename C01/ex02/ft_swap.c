f2f23a285c42b1796e75c724f60ebd0cdc36c1de
Fearioh <alexis_delattre@outlook.fr>
2024-04-24 11:45:03 +0200

    ex02

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,35 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelat <adelat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/24 11:33:51 by adelat            #+#    #+#             */
-/*   Updated: 2024/04/24 11:38:32 by adelat           ###   ########.fr       */
+/*   Created: 2024/04/24 11:41:15 by adelat            #+#    #+#             */
+/*   Updated: 2024/04/24 11:44:26 by adelat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_ultimate_ft(int *********nbr)
+void	ft_swap(int *a, int *b)
 {
-	*********nbr = 42;
+	int	i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
 }
 
 int	main(void)
 {
-	int nbr = 0;
-	int	*n1 = &nbr;
-	int **n2 = &n1;
-	int ***n3 = &n2;
-	int ****n4 = &n3;
-	int *****n5 = &n4;
-	int ******n6 = &n5;
-	int *******n7 = &n6;
-	int ********n8 = &n7;
-	int *********n9 = &n8;
-
-	ft_ultimate_ft(n9);
-	printf("%d", nbr);
+	int	i = 0;
+	int	j = 42;
+	int *a = &i;
+	int	*b = &j;
+	
+	ft_swap(a, b);
+	printf("a = %d et b = %d", i, j);
 }
