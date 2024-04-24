71408afe22d69a6baf5dd93ff316455d3238d93e
Fearioh <alexis_delattre@outlook.fr>
2024-04-24 12:46:31 +0200

    ex07

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelat <adelat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 12:28:10 by adelat            #+#    #+#             */
+/*   Updated: 2024/04/24 12:45:25 by adelat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_print_tab(int *tab, int size)
+{
+	int	i;
+
+	i = -1;
+	while (i < size - 1)
+	{
+		i++;
+		printf("%d\n", tab[i]);
+	}
+}
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	stock;
+
+	i = 0;
+	stock = 0;
+	while (i != ((size / 2) + (size % 2)))
+	{
+		stock = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = stock;
+		i++;
+	}
+	ft_print_tab(tab, size);
+}
+
+int	main(void)
+{
+	int	tab[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ft_rev_int_tab(tab, 10);
+}
