66a9fcf1c285a2ef8d4fcc0d5f967ad03c69319b
Fearioh <alexis_delattre@outlook.fr>
2024-04-25 21:49:50 +0200

    ex07 pas mal d'aide sur les pointeurs/tableau/chaine litterale via bing

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelat <adelat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 21:19:10 by adelat            #+#    #+#             */
+/*   Updated: 2024/04/25 21:49:07 by adelat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return str;
+}
+
+int	main(void)
+{
+	char str[17] = "salut les homies";
+	
+	ft_strupcase(str);
+	printf("%s\n", str);
+}
