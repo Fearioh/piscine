85224462c843afb81d5305dbf31c85a778940fe9
Fearioh <alexis_delattre@outlook.fr>
2024-04-25 21:51:54 +0200

    ex08

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelat <adelat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 21:50:38 by adelat            #+#    #+#             */
+/*   Updated: 2024/04/25 21:51:26 by adelat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return str;
+}
+
+int	main(void)
+{
+	char str[17] = "SALUT LES HOMIES";
+	
+	ft_strlowcase(str);
+	printf("%s\n", str);
+}
